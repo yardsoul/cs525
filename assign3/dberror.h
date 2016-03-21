723793b29dd191d932a82282bdfa409035cc6cbc
Adrian Tirados <a.tirados92@gmail.com>
2016-03-21 15:26:46 -0500

    added files

--- assign3/dberror.h
+++ assign3/dberror.h
@@ -27,6 +27,9 @@ typedef int RC;
 #define RC_IM_N_TO_LAGE 302
 #define RC_IM_NO_MORE_ENTRIES 303
 
+#define RC_PINNED_PAGES 400
+#define RC_PAGE_NOT_FOUND 401
+
 /* holder for error messages */
 extern char *RC_message;
 
