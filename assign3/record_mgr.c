bc5ed77313037f88792a6eb1a49ff207c8a5149d
Adrian Tirados <a.tirados92@gmail.com>
2016-03-21 17:07:39 -0500

    added deserialize

--- assign3/record_mgr.c
+++ assign3/record_mgr.c
@@ -1,20 +1,66 @@
 #include "record_mgr.h"
+#include "dberror.h"
+#include "storage_mgr.h"
+
 
 RC initRecordManager (void *mgmtData) {
-
+	return RC_OK;
 }
 
 RC shutdownRecordManager () {
-
+	return RC_OK;
 }
 
 RC createTable (char *name, Schema *schema) {
+	// Create the file
+	createPageFile(name);
 
+	SM_FileHandle fileHandle;
+
+	openPageFile(name, &fileHandle);
+
+
+	ensureCapacity(1, &fileHandle);
+
+	char *schemaToString = serializeSchema(schema);
+
+
+
+	writeBlock(0, &fileHandle, schemaToString);
+
+	closePageFile(&fileHandle);
+	int t;
+	scanf("%d", t);
+	return RC_OK;
 }
 
 RC openTable (RM_TableData *rel, char *name) {
+	BM_BufferPool *bm = (BM_BufferPool *) malloc (sizeof(BM_BufferPool));
+	BM_PageHandle *pageHandle = (BM_PageHandle *) malloc (sizeof(BM_PageHandle));
+
+	initBufferPool(bm, name, 3, RS_FIFO, NULL);
+
+	pinPage(bm, pageHandle, 0);
+
+	serializedSchema = pageHandle->data;
+	deserializedSchema = deserializeSchema(serializedSchema);
+
+	rel->name = name;
+	rel->schema = deserializedSchema;
+	rel->mgmtData = bm;
+
+	free(pageHandle);
+
+	return RC_OK;
 
 }
+
+
+Schema *deserializeSchema (char *serializedSchema) {
+	Schema *deserializedSchema = (Schema *) malloc (sizeof(Schema));
+
+}
+
 
 RC closeTable (RM_TableData *rel) {
 
@@ -27,6 +73,9 @@ RC deleteTable (char *name) {
 int getNumTuples (RM_TableData *rel) {
 
 }
+
+
+
 
 // handling records in a table
 RC insertRecord (RM_TableData *rel, Record *record) {
@@ -66,7 +115,14 @@ int getRecordSize (Schema *schema) {
 }
 
 Schema *createSchema (int numAttr, char **attrNames, DataType *dataTypes, int *typeLength, int keySize, int *keys) {
-
+	Schema *schema = (Schema*) malloc (sizeof(Schema));
+	schema->numAttr = numAttr;
+	schema->attrNames = attrNames;
+	schema->dataTypes = dataTypes;
+	schema->typeLength = typeLength;
+	schema->keySize = keySize;
+	schema->keyAttrs = keys;
+	return schema;
 }
 
 RC freeSchema (Schema *schema) {
